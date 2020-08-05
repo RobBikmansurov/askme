428bcddacf41b0e1a91a3a4a81e707a865cf3f66
Rob Bikmansurov <robb@mail.ru>
2020-08-05 21:54:32 +0500

    add view and route for show users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :users, only: :show
+
+  get 'show' => 'users#show'
+  root to: 'users#show'
+
 end
