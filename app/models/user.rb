ee35a6817ff31fb54eb8538f6bc443af566c9547
Rob Bikmansurov <robb@mail.ru>
2020-08-06 18:52:33 +0500

    add authenticate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,15 +21,15 @@ class User < ApplicationRecord
   validates_confirmation_of :password
 
   def encrypt_password
-    if password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(
-          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
-        )
+    return unless password.present?
+
+    self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+    self.password_hash = User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(
+        password, password_salt, ITERATIONS, DIGEST.length, DIGEST
       )
-      # Оба поля попадут в базу при сохранении (save).
-    end
+    )
+    # Оба поля попадут в базу при сохранении (save).
   end
 
   # Служебный метод, преобразующий бинарную строку в шестнадцатиричный формат, для удобства хранения.
@@ -38,26 +38,16 @@ class User < ApplicationRecord
   end
 
   def self.authenticate(email, password)
-    # Сперва находим кандидата по email
     user = find_by(email: email)
-
-    # Если пользователь не найден, возвращает nil
     return nil unless user.present?
 
-    # Формируем хэш пароля из того, что передали в метод
     hashed_password = User.hash_to_string(
       OpenSSL::PKCS5.pbkdf2_hmac(
         password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
       )
     )
+    return nil unless user.password_hash == hashed_password
 
-    # Обратите внимание: сравнивается password_hash, а оригинальный пароль так
-    # никогда и не сохраняется нигде. Если пароли совпали, возвращаем
-    # пользователя.
-    return user if user.password_hash == hashed_password
-
-    # Иначе, возвращаем nil
-    nil
+    user
   end
-
 end
