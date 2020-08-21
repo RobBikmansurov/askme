329964c9586b31a9b2038f262030eca90f2771a4
Rob Bikmansurov <robb@mail.ru>
2020-08-21 12:42:07 +0500

    add redirect to root when RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
   def reject_user
     redirect_to root_path, alert: 'Вам сюда низя!'
+  end
+
+  def not_found
+    redirect_to root_path, alert: 'Запись не найдена!'
   end
 
   private
