9eacf7a14a1000cf8f26311450c1518437d2983f
Rob Bikmansurov <robb@mail.ru>
2020-09-16 20:58:28 +0500

    security - remove wrong patterns and add right REST for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   resources :questions, except: %i[show new index]
-  resources :sessions, only: %i[new create destroy]
+  resource :session, only: %i[new create destroy]
   resources :tags, param: :name, only: :show
 
   root to: 'users#index'
-
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
 end
